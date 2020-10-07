21362323fbc49e9fab565854811cd3f9f7ae67e3
Break1205 <59571361+Break1205@users.noreply.github.com>
2020-10-08 02:39:10 +0700

    Initial Commit

---
+++ src/main/java/com/fa20se28/vma/VmaApplication.java
@@ -0,0 +1,13 @@
+package com.fa20se28.vma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fa20se28/vma/VmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fa20se28.vma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
