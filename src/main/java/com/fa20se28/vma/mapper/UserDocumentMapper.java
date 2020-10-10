2cd95ba83b63a44ef5b3ac5a775a59369853e0b9
Break1205 <59571361+Break1205@users.noreply.github.com>
2020-10-11 03:20:32 +0700

    Create UserDocumentMapper. (TBA getDetailDocumentOfUser)

---
+++ src/main/java/com/fa20se28/vma/mapper/UserDocumentMapper.java
@@ -0,0 +1,9 @@
+package com.fa20se28.vma.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserDocumentMapper {
+}
