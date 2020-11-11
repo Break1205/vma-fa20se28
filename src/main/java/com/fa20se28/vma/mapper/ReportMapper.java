3e6083c8b477006a826a9e229274d0fd49cf8f20
Break1205 <59571361+Break1205@users.noreply.github.com>
2020-11-12 01:50:16 +0700

    Export Vehicles and Schedule

--- src/main/java/com/fa20se28/vma/mapper/ReportMapper.java
+++ src/main/java/com/fa20se28/vma/mapper/ReportMapper.java
@@ -2,6 +2,7 @@ package com.fa20se28.vma.mapper;
 
 import com.fa20se28.vma.model.Schedule;
 import com.fa20se28.vma.model.ScheduleDetail;
+import com.fa20se28.vma.model.VehicleReport;
 import org.apache.ibatis.annotations.Many;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -57,4 +58,46 @@ public interface ReportMapper {
             @Result(property = "contributorId", column = "contributor_id")
     })
     List<ScheduleDetail> getScheduleDetails(@Param("contractId") String contractId);
+
+    @Select("SELECT \n" +
+            "v.vehicle_id,\n" +
+            "vt.vehicle_type_name, \n" +
+            "b.brand_name, \n" +
+            "u.user_id, \n" +
+            "u.full_name\n" +
+            "FROM vehicle v\n" +
+            "JOIN vehicle_type vt \n" +
+            "ON v.vehicle_type_id = vt.vehicle_type_id \n" +
+            "JOIN brand b \n" +
+            "ON v.brand_id = b.brand_id \n" +
+            "JOIN [user] u \n" +
+            "ON u.user_id = v.owner_id \n" +
+            "WHERE v.vehicle_status != 'DELETED' " +
+            "ORDER BY u.user_id")
+    @Results(id = "vehicleReportResult", value = {
+            @Result(property = "vehicleId", column = "vehicle_id"),
+            @Result(property = "vehicleType", column = "vehicle_type_name"),
+            @Result(property = "brand", column = "brand_name"),
+            @Result(property = "ownerId", column = "user_id"),
+            @Result(property = "ownerName", column = "full_name")
+    })
+    List<VehicleReport> getVehiclesForReport();
+
+    @Select("SELECT COUNT(cv.vehicle_id) \n" +
+            "FROM (\n" +
+            "SELECT \n" +
+            "v.vehicle_id,\n" +
+            "vt.vehicle_type_name, \n" +
+            "b.brand_name, \n" +
+            "u.user_id, \n" +
+            "u.full_name\n" +
+            "FROM vehicle v\n" +
+            "JOIN vehicle_type vt \n" +
+            "ON v.vehicle_type_id = vt.vehicle_type_id \n" +
+            "JOIN brand b \n" +
+            "ON v.brand_id = b.brand_id \n" +
+            "JOIN [user] u \n" +
+            "ON u.user_id = v.owner_id \n" +
+            "WHERE v.vehicle_status != 'DELETED') cv")
+    int getTotalVehicleForReport();
 }
