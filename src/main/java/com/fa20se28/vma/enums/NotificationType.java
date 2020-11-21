59ab94ab25918edca2be987b23f69e4a5dd03f27
Break1205 <59571361+Break1205@users.noreply.github.com>
2020-11-21 12:29:58 +0700

    Config for Android, IOS, WEB notification

--- src/main/java/com/fa20se28/vma/enums/NotificationType.java
+++ src/main/java/com/fa20se28/vma/enums/NotificationType.java
@@ -1,8 +1,11 @@
 package com.fa20se28.vma.enums;
 
 public enum NotificationType {
-    REQUEST_ACCEPTED,
-    REQUEST_DENIED,
-    CONTRACT_COMPLETED,
-    CONTRACT_ASSIGNED
+    REQUEST_ACCEPTED, // mobile
+    REQUEST_DENIED, // mobile
+    CONTRACT_COMPLETED, // web
+    CONTRACT_ASSIGNED, // mobile
+    START_TRIP, // web
+    END_TRIP, // web
+    LICENSE_EXPIRED, // mobile
 }
